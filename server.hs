cedb5eca5c16ee67e9b8c69ae3dd2c91eefaf729
Takahiro HIMURA <taka@himura.jp>
2014-04-19 15:04:36 +0900

    port setting

--- server.hs
+++ server.hs
@@ -1,14 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Network.Wai.Handler.WarpTLS
-import Network.Wai.Handler.Warp (defaultSettings)
+import Network.Wai.Handler.Warp (defaultSettings, setPort)
 import Network.Wai
 import qualified Network.HTTP.Types as HT
+import System.Environment (getArgs)
 
 main :: IO ()
-main = runTLS myTlsSettings defaultSettings app
+main = do
+    (port:_) <- getArgs
+    server (read port)
+
+server :: Int -- ^ port
+       -> IO ()
+server port = runTLS myTlsSettings setting app
   where
     myTlsSettings = tlsSettings "./server.cert" "./server.key"
+    setting = setPort port defaultSettings
 
 app :: Application
 app _req = return $ responseLBS HT.status200 [] ""
